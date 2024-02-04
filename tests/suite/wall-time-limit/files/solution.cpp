f9d690453bc765ee0835f6793930c719d6c12516
sadfun <job@sadfun.org>
2024-02-04 21:53:15 +0100

    add wall-time-limit test

---
+++ tests/suite/wall-time-limit/files/solution.cpp
@@ -0,0 +1,12 @@
+/*
+ *	Process Isolator -- Test Suite
+ *
+ *	(c) 2024 Sort Me <guys@sort-me.org>
+ */
+
+#include <unistd.h>
+
+int main(int argc, char **argv) {
+    // sleep 0.5 seconds
+    usleep(500000);
+}
